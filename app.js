1392d8643e4597fbc9d9e85140f1866758f77333
cparros <celparros@gmail.com>
2021-05-17 23:00:21 -0400

    added get user function to change values of data

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
+//create a vue instance that is allowed because of cdn
 const app = Vue.createApp({
-
+  //data will be used on front end
   data() {
     return {
       firstName: 'Clayton',
@@ -8,8 +9,19 @@ const app = Vue.createApp({
       gender: 'male',
       picture: 'https://randomuser.me/api/portraits/men/10.jpg'
     }
+  },
+  //methods can access any data defined above
+  methods: {
+    getUser() {
+      this.firstName = 'Sam'
+      this.lastName = 'Smith'
+      this.email = 'Sam@yahoo.com'
+      this.gender = 'female'
+      this.picture = 'https://randomuser.me/api/portraits/women/10.jpg'
+    }
   }
   
 })
 
+//mount above data and methods to div with #app id
 app.mount('#app')
